3da11b1b7766cda64d92f37f022ef5be89f07056
Niamh Gowran <91330763+ngowran@users.noreply.github.com>
2022-05-20 01:59:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,18 +12,33 @@ config = {
 };
 
 firebase = pyrebase.initialize_app(config)
-
 storage = firebase.storage()
 database = firebase.database()
+i = 0
 
 while True:
+  
+  # Setting our values
   humidty, temperature = Adafruit_DHT.read_retry(sensor, pin)
   sensor_data = ('Temp={0:0.1f}*C Humidty={1:0.1f}%'.format(temperature, humidty))
+  
+  # Prints in terminal
   print(sensor_data)
-  database.child("Renu")
-  data = {"Key1": sensor_data}
-  database.set(data)
+  
+  # Setting the current time to a variable.
+  now = datetime.now()
+  current_time = now.strftime("%H:%M:%S")
+  
+  # Updating data to main branch.
+  data = {f"Key {i}": f"{sensor_data} Time= {current_time}"}
+  database.child("Renu").update(data)
+  
+  # Setting latest data to individual branch - to be displayed on web/mobile applications.
+  set_data = {"Key 1": f"{sensor_data} Time= {current_time}"}
+  database.child("sensor").set(set_data)
+  
   time.sleep(10)
+  i += 1
 
   # Program runs indefinitely until you stop it manually.
   # Sends data every 10 seconds.
