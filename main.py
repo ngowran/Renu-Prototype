ff31393889716e9609179b8d949a3593f3312ac6
Niamh Gowran <91330763+ngowran@users.noreply.github.com>
2022-05-24 20:56:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import Adafruit_DHT
 import time
 from datetime import datetime
 
+# Initialising our sensor.
 sensor = Adafruit_DHT.DHT11
 
+# Pin the data line on the DHT11 is connected to.
 pin = 4
 
+# Initialising Firebase
 config = {
   # Include here the config information of your Firebase database.
   # I have removed this for security purposes.
@@ -26,8 +29,9 @@ while True:
   # Prints in terminal.
   print(sensor_data)
   
-  # Setting the current time to a variable.
+  # Setting the current date and time to a variable.
   now = datetime.now()
+  current_date = now.strftime("%d:%m:%Y")
   current_time = now.strftime("%H:%M:%S")
   
   # Updating data to main branch.
@@ -38,8 +42,8 @@ while True:
   set_data = {"Key 1": f"{sensor_data} Time= {current_time}"}
   database.child("sensor").set(set_data)
   
-  time.sleep(10)
+  time.sleep(30)
   i += 1
 
   # Program runs indefinitely until you stop it manually.
-  # Sends data every 10 seconds.
+  # Sends data every 30 seconds.
